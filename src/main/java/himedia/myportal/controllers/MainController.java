21db895be8ea0f7c5b846cb0f3635dcfbcb5b2eb
lee <l_lpp@naver.com>
2025-01-14 10:21:56 +0900

    Front Controller 설정파일의 기본값 Root Context Listener

---
+++ src/main/java/himedia/myportal/controllers/MainController.java
@@ -0,0 +1,15 @@
+package himedia.myportal.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainController {
+	@RequestMapping({"/", "/main"})
+	public ModelAndView main() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("/WEB-INF/views/home.jsp");
+		return mav;
+	}
+}
