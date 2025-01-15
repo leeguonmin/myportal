9530bb4ea498ce1eeb0446d78a0098376f1efea4
lee <l_lpp@naver.com>
2025-01-15 15:00:50 +0900

    순서 UserDaoImpl 생성

---
+++ src/main/java/himedia/myportal/repositories/UserDaoImpl.java
@@ -0,0 +1,25 @@
+package himedia.myportal.repositories;
+
+import himedia.myportal.repositories.vo.UserVo;
+
+public class UserDaoImpl implements UserDao {
+
+	@Override
+	public int insert(UserVo vo) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public UserVo selectUser(String email) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public UserVo selectUser(String email, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
