4ded65cabb45839115ed0acc22b7fb2244554bfa
lee <l_lpp@naver.com>
2025-01-15 12:10:30 +0900

    보드서비스Impl 생성

---
+++ src/main/java/himedia/myportal/services/BoardServiceImpl.java
@@ -0,0 +1,39 @@
+package himedia.myportal.services;
+
+import java.util.List;
+
+import himedia.myportal.repositories.vo.BoardVo;
+
+public class BoardServiceImpl implements BoardService {
+
+	@Override
+	public List<BoardVo> getList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BoardVo getContent(Integer no) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean write(BoardVo boardVo) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean update(BoardVo boardVo) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteByNoAndUserNo(Integer no, Long userNo) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
