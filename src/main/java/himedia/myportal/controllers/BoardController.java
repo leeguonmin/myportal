04b05cdd3f4c597377dc2205fbcb5a1d65e57ef7
lee <l_lpp@naver.com>
2025-01-15 12:17:22 +0900

    세팅..

--- src/main/java/himedia/myportal/controllers/BoardController.java
+++ src/main/java/himedia/myportal/controllers/BoardController.java
@@ -1,10 +1,27 @@
 package himedia.myportal.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import himedia.myportal.repositories.vo.BoardVo;
+import himedia.myportal.services.BoardService;
 
 @Controller
 @RequestMapping("/board")
 public class BoardController {
+	@Autowired
+	private BoardService boardServiceImpl;
+	// 하위 처리
+	@RequestMapping({"", "/", "/list"})
+	public String list(Model model) {
+		List<BoardVo> list = boardServiceImpl.getList();
+		model.addAttribute("list", list);
+		
+		return "board/list";
+	}
 
 }
