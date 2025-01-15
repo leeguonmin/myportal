5808844443298f176107059328a8bdb266b93b2c
lee <l_lpp@naver.com>
2025-01-15 14:39:15 +0900

    2순서 userControoler 채웠으ㅜㅁ

--- src/main/java/himedia/myportal/controllers/UsersController.java
+++ src/main/java/himedia/myportal/controllers/UsersController.java
@@ -1,5 +1,20 @@
 package himedia.myportal.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/uesrs")
 public class UsersController {
+	@GetMapping({"", "/", "/join"})
+	public String joinForm() {
+		return "users/joinform";
+	}
+	
+	@GetMapping("/joinsuccess")
+	public String joinSuccess() {
+		return "users/joinsuccess";
+	}
 
 }
