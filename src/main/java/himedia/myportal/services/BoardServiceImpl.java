ceb4d9ab8e6175ad65d752d6422adec921a6ec07
lee <l_lpp@naver.com>
2025-01-15 12:12:41 +0900

    추가

--- src/main/java/himedia/myportal/services/BoardServiceImpl.java
+++ src/main/java/himedia/myportal/services/BoardServiceImpl.java
@@ -15,8 +15,8 @@ public class BoardServiceImpl implements BoardService {
 
 	@Override
 	public List<BoardVo> getList() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return boardDaoImpl.selectAll();
 	}
 
 	@Override
