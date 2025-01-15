b9b29a2ac8a41f5422a980a85099c0c184e214b9
lee <l_lpp@naver.com>
2025-01-15 15:27:55 +0900

    UsersController 에서 join @PostMapping

--- src/main/java/himedia/myportal/controllers/UsersController.java
+++ src/main/java/himedia/myportal/controllers/UsersController.java
@@ -1,12 +1,21 @@
 package himedia.myportal.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import himedia.myportal.repositories.vo.UserVo;
+import himedia.myportal.services.UserService;
 
 @Controller
 @RequestMapping("/users")
 public class UsersController {
+	@Autowired
+	UserService userServiceImpl;
+	
 	@GetMapping({"", "/", "/join"})
 	public String joinForm() {
 		return "users/joinform";
@@ -17,4 +26,16 @@ public class UsersController {
 		return "users/joinsuccess";
 	}
 
+	@PostMapping("/join")
+	public String joinAction(@ModelAttribute UserVo userVo) {
+		boolean success = userServiceImpl.join(userVo);
+		
+		if(!success) {
+			System.err.println("회원가입 실~패~");
+			return "redirect:/users/join";
+		} else {
+			System.out.println("회원가입 성공!!!");
+			return "redirect:/users/joinsuccess";
+		}
+	}
 }
