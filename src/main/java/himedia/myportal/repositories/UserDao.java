1bdeaba1cf7e5c06f1cdb4236544ee6e5569fc92
lee <l_lpp@naver.com>
2025-01-15 14:59:55 +0900

    +임폴트

--- src/main/java/himedia/myportal/repositories/UserDao.java
+++ src/main/java/himedia/myportal/repositories/UserDao.java
@@ -1,5 +1,7 @@
 package himedia.myportal.repositories;
 
+import himedia.myportal.repositories.vo.UserVo;
+
 public interface UserDao {
 	public int insert(UserVo vo);
 	public UserVo selectUser(String email);
