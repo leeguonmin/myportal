aabfafd58c1e1fb71fc42670ec7cdb6aa344eea5
lee <l_lpp@naver.com>
2025-01-15 12:02:56 +0900

    exception 세팅

--- src/main/java/himedia/myportal/exception/BoardDaoException.java
+++ src/main/java/himedia/myportal/exception/BoardDaoException.java
@@ -1,5 +1,36 @@
 package himedia.myportal.exception;
 
+import himedia.myportal.repositories.vo.BoardVo;
+
 public class BoardDaoException extends RuntimeException {
+	private BoardVo boardVo = null;
+
+	public BoardDaoException() {
+		super();
+		
+	}
+
+	public BoardDaoException(String message) {
+		super(message);
+		
+	}
+	
+	public BoardDaoException(String message, BoardVo vo) {
+		super(message);
+		this.boardVo = vo;
+		
+	}
+
+	public BoardVo getBoardVo() {
+		return boardVo;
+	}
+
+	public void setBoardVo(BoardVo boardVo) {
+		this.boardVo = boardVo;
+	}
+	
+	
+	
+	
 
 }
