8f03e83b6a7cac8928fc99827c27edc5eb042f24
lee <l_lpp@naver.com>
2025-01-15 14:45:34 +0900

    순서3 mysql에 테이블 추가하고 테이블 데이터에 맞춰 Vo 생성

---
+++ src/main/java/himedia/myportal/repositories/vo/UserVo.java
@@ -0,0 +1,54 @@
+package himedia.myportal.repositories.vo;
+
+import java.util.Date;
+
+// 마이sql 추가하고 Vo 생성 했음 
+
+
+public class UserVo {
+	private Integer no;
+	private String name;
+	private String email;
+	private String password;
+	private String gender;
+	private Date joinDate;
+	public Integer getNo() {
+		return no;
+	}
+	public void setNo(Integer no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public Date getJoinDate() {
+		return joinDate;
+	}
+	public void setJoinDate(Date joinDate) {
+		this.joinDate = joinDate;
+	}
+	
+	
+
+}
