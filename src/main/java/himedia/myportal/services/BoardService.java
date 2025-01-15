b7a9de898d2d6fc7cbde806cd6dde8771c1e4bdb
lee <l_lpp@naver.com>
2025-01-15 12:09:26 +0900

    서비스 생성

---
+++ src/main/java/himedia/myportal/services/BoardService.java
@@ -0,0 +1,14 @@
+package himedia.myportal.services;
+
+import java.util.List;
+
+import himedia.myportal.repositories.vo.BoardVo;
+
+public interface BoardService {
+	public List<BoardVo> getList();
+	public BoardVo getContent(Integer no);
+	public boolean write(BoardVo boardVo);
+	public boolean update(BoardVo boardVo);
+	public boolean deleteByNoAndUserNo(Integer no, Long userNo);
+
+}
