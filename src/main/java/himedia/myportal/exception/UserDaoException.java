7c8e63a2f577f03a8df92b7c487a30438aef72ae
lee <l_lpp@naver.com>
2025-01-15 14:51:48 +0900

    4순서 UserDaoException 만들고 채웠음

--- src/main/java/himedia/myportal/exception/UserDaoException.java
+++ src/main/java/himedia/myportal/exception/UserDaoException.java
@@ -1,5 +1,23 @@
 package himedia.myportal.exception;
 
-public class UserDaoException {
+import himedia.myportal.repositories.vo.UserVo;
+
+public class UserDaoException extends RuntimeException {
+	private UserVo userVo = null;
+	
+	// 기본 생성자 
+	public UserDaoException() {
+		
+	}
+	
+	// 에러메세지 생성자 
+	public UserDaoException(String message) {
+		super(message);
+	}
+	
+	public UserDaoException(String message, UserVo userVo) {
+		super(message);
+		this.userVo = userVo;
+	}
 
 }
