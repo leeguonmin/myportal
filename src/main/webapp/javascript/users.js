1ee2d164b410e184ba913bd290601692ebc2cc55
lee <l_lpp@naver.com>
2025-01-16 10:08:11 +0900

    과정2 js 코드 채우는 중

--- src/main/webapp/javascript/users.js
+++ src/main/webapp/javascript/users.js
@@ -5,5 +5,10 @@
 function checkEmail(event) {
 	// 가입폼의 #check-email을 누르면 email 텍스트 상자의 value가 중복되었는지 
 	// Ajax로 체크!
+	alert("이메일 체크 이벤트")
 	
 }
+
+window.addEventListener("load", (event) => {
+	document.getElementById("checkEmail").addEventListener("click", checkEmail);
+})
