fe03915874c0e5988e89bcbf3da8fd348924e50f
lee <l_lpp@naver.com>
2025-01-16 10:42:43 +0900

    UserServiceImpl의 getUser

--- src/main/java/himedia/myportal/services/UserServiceImpl.java
+++ src/main/java/himedia/myportal/services/UserServiceImpl.java
@@ -28,8 +28,8 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public UserVo getUser(String email) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return userDaoImpl.selectUser(email);
 	}
 
 	@Override
