cc1fcdd897a155e8ef37acfb2faa832b7c4c3614
lee <l_lpp@naver.com>
2025-01-16 10:50:00 +0900

    UsersController에 추가

--- src/main/java/himedia/myportal/controllers/UsersController.java
+++ src/main/java/himedia/myportal/controllers/UsersController.java
@@ -1,5 +1,8 @@
 package himedia.myportal.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import himedia.myportal.repositories.vo.UserVo;
 import himedia.myportal.services.UserService;
@@ -79,6 +83,34 @@ public class UsersController {
 		
 		return "redirect:/";
 	}
+	
+	
+	@ResponseBody
+	@GetMapping("/checkEmail")
+	// JSON API로 받아오기로 했쥬
+	public Object exists(@RequestParam(value="email", required=false, defaultValue="") String email) {
+		// { "result": "success", "data": true } 
+		UserVo vo = userServiceImpl.getUser(email);
+		boolean exists = vo != null ? true: false;
+		
+		Map<String, Object> map= new HashMap<>();
+		map.put("result",  "success");
+		map.put("data",  exists);
+		
+		return map;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
