2f696aea3d1f9cbb8dd5be079375b8f05458e799
lee <l_lpp@naver.com>
2025-01-17 13:06:40 +0900

    여기까지하고 스프링서블릿 수정할거임

--- src/main/java/himedia/myportal/interceptors/LogoutInterceptor.java
+++ src/main/java/himedia/myportal/interceptors/LogoutInterceptor.java
@@ -1,18 +1,32 @@
 package himedia.myportal.interceptors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class LogoutInterceptor implements HandlerInterceptor {
+	private static final Logger logger = LoggerFactory.getLogger(LogoutInterceptor.class);
 
+	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		// TODO Auto-generated method stub
-		return HandlerInterceptor.super.preHandle(request, response, handler);
+		logger.debug("LogoutInterceptor.preHandle calls");
+		
+		// 세션 무효화
+		request.getSession().invalidate();
+		
+		// 무효화 시켰으니까, 홈페이지 쪽으로 돌려줘(리다이렉트)
+		response.sendRedirect(request.getContextPath());
+		
+		
+		return false;
+		// false면 뒤로 넘어가지 않지!
 	}
+	
 	
 
 }
