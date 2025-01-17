dfda72dbc83da03e8590dd66787e638049270015
lee <l_lpp@naver.com>
2025-01-17 13:02:11 +0900

    소스에 오버라이딩

--- src/main/java/himedia/myportal/interceptors/LogoutInterceptor.java
+++ src/main/java/himedia/myportal/interceptors/LogoutInterceptor.java
@@ -2,6 +2,17 @@ package himedia.myportal.interceptors;
 
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 public class LogoutInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// TODO Auto-generated method stub
+		return HandlerInterceptor.super.preHandle(request, response, handler);
+	}
+	
 
 }
