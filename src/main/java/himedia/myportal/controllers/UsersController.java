f6f8b8c2e9ec3fe5cd691410f5fcee542334dd93
lee <l_lpp@naver.com>
2025-01-20 10:35:20 +0900

    검증 결과 추가

--- src/main/java/himedia/myportal/controllers/UsersController.java
+++ src/main/java/himedia/myportal/controllers/UsersController.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,7 +38,9 @@ public class UsersController {
 	}
 
 	@PostMapping("/join")
-	public String joinAction(@ModelAttribute @Valid UserVo userVo) {
+	public String joinAction(@ModelAttribute @Valid UserVo userVo, 
+			BindingResult result,		//검증 결과
+			Model model) {
 		boolean success = userServiceImpl.join(userVo);
 		
 		if(!success) {
